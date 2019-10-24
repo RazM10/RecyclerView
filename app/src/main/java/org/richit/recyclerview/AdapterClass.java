6af042ecbad39b3ea933036e5209ea8aede16980
Razaul Munir <razaulmunir@gmail.com>
2019-10-24 10:37:28 +0600

    added parent list item on click

--- app/src/main/java/org/richit/recyclerview/AdapterClass.java
+++ app/src/main/java/org/richit/recyclerview/AdapterClass.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,22 +17,31 @@ public class AdapterClass extends RecyclerView.Adapter<AdapterClass.ViewHolderCl
 
     private String[] data;
     private List<Person> personList;
-    public AdapterClass(List<Person> personList){
-        this.personList=personList;
+
+    public AdapterClass(List<Person> personList) {
+        this.personList = personList;
     }
+
     @NonNull
     @Override
     public ViewHolderClass onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        LayoutInflater inflater=LayoutInflater.from(parent.getContext());
-        View view=inflater.inflate(R.layout.list_item,parent,false);
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.list_item, parent, false);
         return new ViewHolderClass(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolderClass holder, int position) {
-        String title= personList.get(position).getName();
+        String title = personList.get(position).getName();
         holder.title_tv.setText(title);
+
+        holder.linearLayoutParent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
@@ -39,15 +49,18 @@ public class AdapterClass extends RecyclerView.Adapter<AdapterClass.ViewHolderCl
         return personList.size();
     }
 
-    public class ViewHolderClass extends RecyclerView.ViewHolder{
+    public class ViewHolderClass extends RecyclerView.ViewHolder {
 
         ImageView imgIcon;
         TextView title_tv;
+        LinearLayout linearLayoutParent;
+
         public ViewHolderClass(@NonNull View itemView) {
             super(itemView);
 
-            imgIcon=itemView.findViewById(R.id.imgIcon);
-            title_tv=itemView.findViewById(R.id.title_tv);
+            imgIcon = itemView.findViewById(R.id.imgIcon);
+            title_tv = itemView.findViewById(R.id.title_tv);
+            linearLayoutParent = itemView.findViewById(R.id.itemParent);
         }
     }
 }
